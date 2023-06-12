5988c4d0e4e38ad3a572a50e3460af450c8220c4
willsth <wls@braincapture.dk>
2023-06-12 13:29:39 +0200

    refine code layout

--- bayesian_utils/methods.py
+++ bayesian_utils/methods.py
@@ -44,23 +44,23 @@ class KF:
         return state_estimates, cov_estimates
     
     # Rauch-Tung-Striebel (RTS) Smoother
-    def smoother(self, states_estimates, cov_estimates):
-        n = len(states_estimates)
-        states_estimates_smoothed = np.empty((n, self.dim_m))
-        states_estimates_smoothed[-1] = states_estimates[-1]
+    def smoother(self, state_estimates, cov_estimates):
+        n = len(state_estimates)
+        state_estimates_smoothed = np.empty((n, self.dim_m))
+        state_estimates_smoothed[-1] = state_estimates[-1]
         
         cov_estimates_smoothed = np.empty((n, self.dim_m, self.dim_m))
         cov_estimates_smoothed[-1] = cov_estimates[-1]
 
         for k in range(n- 2, -1, -1):
-            m_pred = self.A @ states_estimates[k]
+            m_pred = self.A @ state_estimates[k]
             P_pred = self.A @ cov_estimates[k] @ self.A.T + self.Q
             G = cov_estimates[k] @ self.A.T @ np.linalg.inv(P_pred)
             
-            states_estimates_smoothed[k] = states_estimates[k] + G @ (states_estimates_smoothed[k + 1] - m_pred)
+            state_estimates_smoothed[k] = state_estimates[k] + G @ (state_estimates_smoothed[k + 1] - m_pred)
             cov_estimates_smoothed[k] = cov_estimates[k] + G @ (cov_estimates_smoothed[k + 1] - P_pred) @ G.T
             
-        return states_estimates_smoothed, cov_estimates_smoothed
+        return state_estimates_smoothed, cov_estimates_smoothed
     
 class EKF:
     def __init__(self, f, F_jacobian, h, H_jacobian, Q, R, dim_m = 4, dim_y = 2):
@@ -89,11 +89,13 @@ class EKF:
         
         return m, P
 
-    # Extended Kalman Filtering
+    # Extended Kalman Filtering (EFK)
     def filter(self, measurements, m = None, P = None):
         n = len(measurements)
+
         if m is None: m = np.zeros(self.dim_m)
         if P is None: P = np.eye(self.dim_m)
+
         state_estimates = np.empty((n, self.dim_m))
         cov_estimates = np.empty((n, self.dim_m, self.dim_m))
         
@@ -105,24 +107,25 @@ class EKF:
             
         return state_estimates, cov_estimates
     
-    # Rauch-Tung-Striebel (RTS) Smoother
-    def smoother(self, states_estimates, cov_estimates):
-        n = len(states_estimates)
-        states_estimates_smoothed = np.empty((n, self.dim_m))
-        states_estimates_smoothed[-1] = states_estimates[-1]
+    # Extended Rauch-Tung-Striebel (ERTS) Smoother
+    def smoother(self, state_estimates, cov_estimates):
+        n = len(state_estimates)
+
+        state_estimates_smoothed = np.empty((n, self.dim_m))
+        state_estimates_smoothed[-1] = state_estimates[-1]
         
         cov_estimates_smoothed = np.empty((n, self.dim_m, self.dim_m))
         cov_estimates_smoothed[-1] = cov_estimates[-1]
 
         for k in range(n-2, -1, -1):
-            F = self.F_jacobian(states_estimates[k])
-            m_pred = self.f(states_estimates[k])
+            F = self.F_jacobian(state_estimates[k])
+            m_pred = self.f(state_estimates[k])
             P_pred = F @ cov_estimates[k] @ F.T + self.Q
             G = cov_estimates[k] @ F.T @ np.linalg.inv(P_pred)
-            states_estimates_smoothed[k] = states_estimates[k] + G @ (states_estimates_smoothed[k+1] - m_pred)
+            state_estimates_smoothed[k] = state_estimates[k] + G @ (state_estimates_smoothed[k+1] - m_pred)
             cov_estimates_smoothed[k] = cov_estimates[k] + G @ (cov_estimates_smoothed[k+1] - P_pred) @ G.T
             
-        return states_estimates_smoothed, cov_estimates_smoothed
+        return state_estimates_smoothed, cov_estimates_smoothed
     
 
 class UFK:
@@ -192,11 +195,13 @@ class UFK:
 
         return m, P
 
+    # Unscented Kalman Filtering (UKF)
     def filter(self, measurements, m = None, P = None):
         n_measurements = len(measurements)
 
         if m is None: m = np.zeros(self.dim_m)
         if P is None: P = np.eye(self.dim_m)
+        
         state_estimates = np.empty((n_measurements, self.dim_m))
         cov_estimates = np.empty((n_measurements, self.dim_m, self.dim_m))
 
@@ -210,6 +215,7 @@ class UFK:
         
         return state_estimates, cov_estimates
 
+    # Unscented Rauch-Tung-Striebel (URTS) Smoother
     def smoother(self, state_estimates, cov_estimates):
         n = len(state_estimates)
 
