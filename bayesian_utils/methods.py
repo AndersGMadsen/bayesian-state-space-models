8a0cb4c9faa6212e8066ab753ab12be919c24a6e
willsth <wls@braincapture.dk>
2023-06-12 13:13:25 +0200

    add UFK class

--- bayesian_utils/methods.py
+++ bayesian_utils/methods.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import cholesky
 
 class KF:
     def __init__(self, A, Q, H, R, dim_m = 4, dim_y = 2):
@@ -122,3 +123,117 @@ class EKF:
             cov_estimates_smoothed[k] = cov_estimates[k] + G @ (cov_estimates_smoothed[k+1] - P_pred) @ G.T
             
         return states_estimates_smoothed, cov_estimates_smoothed
+    
+
+class UFK:
+    def __init__(self, f, Q, h, R, dim_m = 4, dim_y = 2, alpha=0.5, beta=2, kappa=0):
+        self.f = f
+        self.Q = Q
+        self.h = h
+        self.R = R
+        self.dim_m = dim_m
+        self.dim_y = dim_y
+        self.alpha = alpha
+        self.beta = beta
+        self.kappa = kappa
+        
+        assert Q.shape == (dim_m, dim_m)
+        assert R.shape == (dim_y, dim_y)
+
+    def weights(self, n):
+            
+        lambda_ = self.alpha**2 * (n + self.kappa) - n
+
+        Wc = np.full(2*n + 1, 1 / (2*(n + lambda_)))
+        Wm = Wc.copy()
+
+        Wc[0] = lambda_ / (n + lambda_) + (1 - self.alpha**2 + self.beta)
+        Wm[0] = lambda_ / (n + lambda_)
+        
+        return Wm, Wc
+
+    def sigmas(self, n, m, P):
+
+        sigmas = np.empty((2*n + 1, n))
+
+        lambda_ = self.alpha**2 * (n + self.kappa) - n
+        U = cholesky((n + lambda_) * P)
+
+        sigmas[0] = m
+        for i in range(n):
+            sigmas[i + 1] = m + U[i]
+            sigmas[n + i + 1] = m - U[i]
+
+        return sigmas
+
+    def predict(self, m, P, Wm, Wc):
+        
+        sigmas = self.sigmas(self.dim_m, m, P)
+        sigmas_transformed = np.array([self.f(sigma) for sigma in sigmas])
+        
+        m_pred = np.sum(Wm[:, None] * sigmas_transformed, axis=0)
+        P_pred = np.sum([Wc[i] * np.outer(sigmas_transformed[i] - m_pred, sigmas_transformed[i] - m_pred) for i in range(len(Wc))], axis=0) + self.Q
+
+        return m_pred, P_pred
+
+    def update(self, m_pred, P_pred, y, Wm, Wc):
+
+        sigmas = self.sigmas(self.dim_m, m_pred, P_pred)
+        sigmas_transformed = np.array([self.h(sigma) for sigma in sigmas])
+        
+        y_pred = np.sum(Wm[:, None] * sigmas_transformed, axis=0)
+        S = np.sum([Wc[i] * np.outer(sigmas_transformed[i] - y_pred, sigmas_transformed[i] - y_pred) for i in range(len(Wc))], axis=0) + self.R
+        C = np.sum([Wc[i] * np.outer(sigmas[i] - m_pred, sigmas_transformed[i] - y_pred) for i in range(len(Wc))], axis=0)
+
+        K = C @ np.linalg.inv(S)
+        
+        m = m_pred + K @ (y - y_pred)
+        P = P_pred - K @ S @ K.T
+
+        return m, P
+
+    def filter(self, measurements, m = None, P = None):
+        n_measurements = len(measurements)
+
+        if m is None: m = np.zeros(self.dim_m)
+        if P is None: P = np.eye(self.dim_m)
+        state_estimates = np.empty((n_measurements, self.dim_m))
+        cov_estimates = np.empty((n_measurements, self.dim_m, self.dim_m))
+
+        Wm, Wc = self.weights(self.dim_m)
+        
+        for i, y in enumerate(measurements):
+            m_pred, P_pred = self.predict(m, P, Wm, Wc)
+            m, P = self.update(m_pred, P_pred, y, Wm, Wc)
+            state_estimates[i] = m
+            cov_estimates[i] = P            
+        
+        return state_estimates, cov_estimates
+
+    def smoother(self, state_estimates, cov_estimates):
+        n = len(state_estimates)
+
+        state_estimates_smoothed = np.empty((n, self.dim_m))
+        state_estimates_smoothed[-1] = state_estimates[-1]
+        
+        cov_estimates_smoothed = np.empty((n, self.dim_m, self.dim_m))
+        cov_estimates_smoothed[-1] = cov_estimates[-1]
+
+        Wm, Wc = self.weights(self.dim_m)
+
+        for k in range(n- 2, -1, -1):
+            m = state_estimates[k]
+            P = cov_estimates[k]
+
+            sigmas = self.sigmas(self.dim_m, m, P)
+            sigmas_transformed = np.array([self.f(sigma) for sigma in sigmas])
+
+            m_pred = np.sum(Wm[:, None] * sigmas_transformed, axis=0)
+            P_pred = np.sum([Wc[i] * np.outer(sigmas_transformed[i] - m_pred, sigmas_transformed[i] - m_pred) for i in range(len(Wc))], axis=0) + self.Q
+            D = np.sum([Wc[i] * np.outer(sigmas[i] - m, sigmas_transformed[i] - m_pred) for i in range(len(Wc))], axis=0)
+            G = D @ np.linalg.inv(P_pred)
+
+            state_estimates_smoothed[k] = m + G @ (state_estimates_smoothed[k + 1] - m_pred)
+            cov_estimates_smoothed[k] = P + G @ (cov_estimates_smoothed[k + 1] - P_pred) @ G.T
+
+        return state_estimates_smoothed, cov_estimates_smoothed
