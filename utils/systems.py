b0798ee16141da66013946bac0d4128b1fbc4669
Anders G. Madsen <andersgj.madsen@gmail.com>
2023-06-21 12:03:29 +0200

    Add comments to systems.

--- utils/systems.py
+++ utils/systems.py
@@ -9,6 +9,9 @@ from os.path import exists
 import pickle
 
 
+"""
+Trajectory class using a linear state space model to generate a trajectory for the vehicle.
+"""
 class CarTrajectoryLinear:
     def __init__(self, Q, R, A, H, N = 100):
         self.Q = Q
@@ -33,7 +36,10 @@ class CarTrajectoryLinear:
             self.y[i] = mvn(mean = self.H @ self.x[i], cov = self.R).rvs(1)
         
         return self.x, self.y
-    
+
+"""
+Trajectory class using a nonlinear state space model to generate a trajectory for the vehicle.
+"""
 class CarTrajectoryNonLinear:
     def __init__(self, Q, R, f, h, N = 100):
         self.Q = Q
@@ -58,29 +64,29 @@ class CarTrajectoryNonLinear:
             self.y[i] = self.h(self.x[i]) + mvn(np.zeros(2), self.R).rvs(1)
         
         return self.x, self.y
-    
-    
+
+"""
+Trajectory class using Model Predictive Control to generate a trajectory for the vehicle.
+"""    
 class MPCTrajectory:
-    def __init__(self, x_points, y_points, line_segments, savepath=None):
+    def __init__(self, x_points, y_points, line_segments, sp_reduction = False, savepath=None):
 
         self.x_points = x_points
         self.y_points = y_points
 
         self.line_segments = line_segments
+        
         self._states = None
         self._measurements = None
 
-        self.cx = None
-        self.cy = None
+
         self.states_hist = None
         self.controls_hist = None
-        
-        s1 = 2
-        s2 = 2
                 
-        self.R = np.array([[s1, 0],
-                    [0, s2]])
+        self.R = np.array([[2, 0],
+                    [0, 2]])
         
+        self.sp_reduction = sp_reduction
         self.savepath = savepath
 
     @property
@@ -111,7 +117,9 @@ class MPCTrajectory:
             self._calculate_measurements()
         return self._measurements
     
-    # Based on gut feelings
+    # A function to further reduce the speed of the car in the corners
+    # This is done by reducing the speed in the corners by a factor of 1.5
+    # The method is pretty crude, based on testing and visual inspection, but it works
     def speed_reduction(self, cyaw, sp):
         speed_reduction = np.diff(cyaw)
         speed_reduction = np.concatenate([speed_reduction[int(len(speed_reduction)*0.05):],
@@ -127,20 +135,21 @@ class MPCTrajectory:
         return sp_new
 
     def _calculate_states(self):
-        self.cx, self.cy, cyaw, ck, s = cubic_spline_planner.calc_spline_course(self.x_points, self.y_points, ds=0.1)
+        cx, cy, cyaw, ck, s = cubic_spline_planner.calc_spline_course(self.x_points, self.y_points, ds=0.1)
         cyaw = np.abs(cyaw)
         
-        initial_state = Vehicle(x=self.cx[0], y=self.cy[0], yaw=cyaw[0], v=0.0)
+        initial_state = Vehicle(x=cx[0], y=cy[0], yaw=cyaw[0], v=0.0)
         dl = 1.0
                 
         simulation = Simulation(initial_state, goal_speed=0.5, target_speed=3, max_time = 1000)
         
         # Speed profile
-        sp = np.abs(simulation.calc_speed_profile(self.cx, self.cy, cyaw))
-        sp = self.speed_reduction(cyaw, sp)
+        sp = np.abs(simulation.calc_speed_profile(cx, cy, cyaw))
+        if self.sp_reduction:
+            sp = self.speed_reduction(cyaw, sp)
         
         # Simulation
-        self.states_hist, self.controls_hist = simulation.simulate(self.cx, self.cy, cyaw, ck, dl, sp=sp)
+        self.states_hist, self.controls_hist = simulation.simulate(cx, cy, cyaw, ck, dl, sp=sp)
 
         # Get simulation states
         x, y, v, yaw = self.states_hist['x'], self.states_hist['y'], self.states_hist['v'], self.states_hist['yaw']        
@@ -154,6 +163,7 @@ class MPCTrajectory:
 
         return np.c_[x, y, dx, dy]
 
+    # Measurement transition function
     def h(self, x):
         H = np.array([[1, 0, 0, 0],
                         [0, 1, 0, 0]])
@@ -168,6 +178,7 @@ class MPCTrajectory:
         
         return out
     
+    # Calculate measurements by passing states through h(x) and adding gaussian noise
     def _calculate_measurements(self):
         measurements = self.h(self._states) + mvn([0, 0], self.R).rvs(len(self._states))        
         self._measurements = measurements
@@ -175,6 +186,7 @@ class MPCTrajectory:
     def get_data(self):
         return self.states.copy(), self.measurements.copy()
     
+    # Find the bounding box of the track for plotting
     def get_bounding_box(self):
         min_x, max_x = np.inf, -np.inf
         min_y, max_y = np.inf, -np.inf
@@ -229,7 +241,6 @@ class MPCTrajectory:
             ax.plot(self.x_points, self.y_points, "kx", markersize=10)
             ax.plot(self.states[:i, 0], self.states[:i, 1], "-r", label="trajectory")
             ax.plot(self.measurements[:i, 0], self.measurements[:i, 1], 'bx', markersize=3, label="measurements")
-            #ax.plot(self.cx[target_inds[i]], self.cy[target_inds[i]], "xg", label="target")
 
             plot_car(ax, self.states[i, 0], self.states[i, 1], yaw[i], steer=d[i])
 
